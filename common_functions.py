5e89186069c2445e725599ffec1e185d84797e3b
Rami3sam <rami3sam@gmail.com>
2020-01-20 15:11:21 -0500

    done some code refactoring

--- common_functions.py
+++ common_functions.py
@@ -2,6 +2,11 @@ import PIL
 import os
 import pickle
 import re
+import numpy as np
+import cv2
+import torch
+
+
 def detectFace(mtcnn,image,filename):
     aligned ,_ = mtcnn(image,return_prob=True,save_path=filename);
     boxes,prob = mtcnn.detect(image)
@@ -10,9 +15,9 @@ def detectFace(mtcnn,image,filename):
     else:
         print("no faces detected")
     return boxes,aligned
-all_embeddings = dict()
 
 def loadEmbeddings():
+    all_embeddings = dict()
     for (dirpath, dirnames, filenames) in os.walk("students_photos"):
         if re.match(r'^students_photos/\w*$',dirpath):
             embeddingsPath = os.path.join(dirpath ,'embeddings')
@@ -21,3 +26,46 @@ def loadEmbeddings():
                 student_embedding = pickle.load(embeddingsFile)
                 all_embeddings[student_id] = student_embedding
     return all_embeddings
+
+def drawOnFrame(i,frame,boxes,student_id):
+    boxes_int = np.array(boxes,dtype='int')
+    #calculating coordinates for the border rectangle 
+    startpoint = (boxes_int[i][0],boxes_int[i][1]) #x0 y0
+    endpoint = (boxes_int[i][2],boxes_int[i][3]) #x1 y1
+    cv2.rectangle(frame,startpoint,endpoint,(0,0,255),2)  
+    print('face {} : coordinates {} , {}'.format(i,startpoint,endpoint))
+    #calculating coordiantes for the label rectangle
+    startpoint = (boxes_int[i][0],boxes_int[i][3]) #x0 y1
+    label_height = ((boxes_int[i][3] - boxes_int[i][1]) // 4)
+    endpoint = (boxes_int[i][2],boxes_int[i][3] + label_height) #x1 y1+h
+    cv2.rectangle(frame,startpoint,endpoint,(0,0,255),-1) 
+
+    #textOrigin starting bottom left point and a bottom margin
+    textOrigin = (startpoint[0] ,endpoint[1] - label_height // 5) 
+    fontscale = label_height /40 
+    cv2.putText(frame,student_id,textOrigin,cv2.FONT_HERSHEY_COMPLEX,fontscale,(255,255,255))
+
+
+    #caculate face embedding
+def calculateEmbeddingsErrors(resnet,device,aligned,all_embeddings):
+    #torch.stack method accepts python array not tuples 
+    aligned = [i for i in aligned]
+    aligned = torch.stack(aligned).to(device)
+    calculatedEmbeddings = resnet(aligned).detach().cpu()
+
+    #print('embeddings: {}'.format(calculatedEmbeddings))
+    dist_dict = dict()
+    for key in all_embeddings:
+        dist_arr = []
+        for embeddings in all_embeddings[key]:
+            dist = (calculatedEmbeddings - embeddings).norm().item()
+            dist_arr.append (dist)
+        print('{} distances are: {}'.format(key ,dist_arr))
+        min_dist = min(dist_arr)  
+        dist_dict[min_dist] = key
+        print('{} minimum  distance is : {}'.format(key , min_dist) ,'')
+    min_dist = min(dist_dict.keys())
+    nearest_student = dist_dict.get(min_dist)
+    print(' student is likely to be: {} '.format(nearest_student).center(80,'*'))
+    print(' least distance is: {} '.format(min_dist).center(80,'*'))
+    return min_dist,nearest_student
--- main.py
+++ main.py
@@ -7,82 +7,44 @@ import PIL
 import time
 from common_functions import detectFace
 from common_functions import loadEmbeddings
+from common_functions import drawOnFrame
+from common_functions import calculateEmbeddingsErrors
 #tests if torch library can use gpu if not not it chooses cpu
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('Running on device: {}'.format(device))
 
-#initializes MTCNN face detector, we want MTCNN to detect more than one face => keep_all = True 
-#
-all_embeddings = loadEmbeddings()
+#initialize MTCNN face detector
 
-print('loaded students: ' , *all_embeddings.keys())
-print('*'*30)
+
 mtcnn = MTCNN(
         image_size=160, margin=0, min_face_size=20,
         thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
         device=device,
         keep_all=True,
 )
-
+#Initialize ResNet Inception Model
 resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
 
+#Load precalculated students embeddings
 all_embeddings = loadEmbeddings()
+print('loaded students: ' , *all_embeddings.keys())
+print('*'*80)
 
-#
+#Select camera with ID 0 as a capture device
 capture = cv2.VideoCapture(0)
 while(True):
      
     ret, frame = capture.read()
   
-    frame_PIL = PIL.Image.fromarray(frame)
-    boxes,aligned = detectFace(mtcnn,frame_PIL,'detected_faces/face.jpg')
+    image = PIL.Image.fromarray(frame)
+    boxes,aligned = detectFace(mtcnn,image,'detected_faces/face.jpg')
 
-    #draw a red border around detected faces
     if boxes is not None:
-
         for i in range(0,len(boxes)):
-            boxes_int = np.array(boxes,dtype='int')
-            #calculating coordinates for the border rectangle 
-            startpoint = (boxes_int[i][0],boxes_int[i][1]) #x0 y0
-            endpoint = (boxes_int[i][2],boxes_int[i][3]) #x1 y1
-            cv2.rectangle(frame,startpoint,endpoint,(0,0,255),2)  
-            print('face {} : coordinates {} , {}'.format(i,startpoint,endpoint))
-            #calculating coordiantes for the label rectangle
-            startpoint = (boxes_int[i][0],boxes_int[i][3]) #x0 y1
-            label_height = ((boxes_int[i][3] - boxes_int[i][1]) // 4)
-            endpoint = (boxes_int[i][2],boxes_int[i][3] + label_height) #x1 y1+h
-            cv2.rectangle(frame,startpoint,endpoint,(0,0,255),-1) 
-
-            #textOrigin starting bottom left point and a bottom margin
-            textOrigin = (startpoint[0] ,endpoint[1] - label_height // 5) 
-            fontscale = label_height /40 
-            cv2.putText(frame,'#00000',textOrigin,cv2.FONT_HERSHEY_COMPLEX,fontscale,(255,255,255))
-            
-            #caculate face embedding
-
-            #torch.stack method accepts python array not tuples 
-            aligned = [i for i in aligned]
-            aligned = torch.stack(aligned).to(device)
-            calculatedEmbeddings = resnet(aligned).detach().cpu()
-
-            #print('embeddings: {}'.format(calculatedEmbeddings))
-            dist_dict = dict()
-            for key in all_embeddings:
-                dist_arr = []
-                for embeddings in all_embeddings[key]:
-                    dist = (calculatedEmbeddings - embeddings).norm().item()
-                    dist_arr.append (dist)
-                print('{} distances are: {}'.format(key ,dist_arr))
-                min_dist = min(dist_arr)  
-                dist_dict[min_dist] = key
-                print('{} minimum  distance is : {}'.format(key , min_dist) ,'')
-            min_dist = min(dist_dict.keys())
-            nearest_student = dist_dict.get(min_dist)
-            print(' student is likely to be: {} '.format(nearest_student).center(80,'*'))
-            probability = (2 - min_dist)*100
-            print(' degree of certainty is: {}% '.format('DD.D').center(80,'*'))
+            calculateEmbeddingsErrors(resnet,device,aligned,all_embeddings)
+            drawOnFrame(i,frame,boxes,"#????")
     cv2.imshow('Infinity Project', frame)
-    time.sleep(0.2)
+    time.sleep(1/10) #1/fps
 	#to break from main loop if user presses ESC
     if cv2.waitKey(1) == 27:
         break
